0be726730c04ff9fff91f64e551de2f4d2f2c262
Travis Furrer <tfurrer@nvidia.com>
2022-08-04 10:33:03 -0700

    Modify template to reproduce issue.

---
+++ moduleA/src/main/java/packageA/ClassA.java
@@ -0,0 +1,11 @@
+package packageA;
+
+public class ClassA
+{
+    private ClassA() {}
+
+    /**
+     * This method does nothing.
+     */
+    public static void doNothing() {}
+}
---
+++ moduleB/src/main/java/packageB/ClassB.java
@@ -0,0 +1,11 @@
+package packageB;
+
+public class ClassB
+{
+    private ClassB() {}
+
+    /**
+     * This method does nothing.
+     */
+    public static void doNothing() {}
+}
